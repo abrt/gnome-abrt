3815692e7aaa7a6d5af86a2cebc840a376df1e6b
Jakub Filak <jfilak@redhat.com>
2013-02-22 14:32:34 +0100

    Try harder when looking for icon and don't cache weak results
    - closes #11 #12
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- src/gnome_abrt/application.py
+++ src/gnome_abrt/application.py
@@ -18,11 +18,13 @@
 import os
 import sys
 import logging
+from itertools import ifilter
 
 # PyGObject
 import gi
 from gi.repository import Gtk
 from gi.repository import Gio
+from gi.repository import GdkPixbuf
 
 class Application(object):
 
@@ -42,23 +44,63 @@ __globa_app_cache__ = {}
 
 def compare_executable(executable, de):
     dexec = de.get_executable()
-    return os.path.basename(executable) == os.path.basename(dexec) or executable == dexec
+
+    realpath = None
+    if executable[0] == '/' and os.path.islink(executable):
+        realpath = os.readlink(executable)
+
+        if realpath[0] != '/':
+            realpath = os.path.abspath(os.path.join(os.path.dirname(executable), realpath))
+
+        if realpath == executable:
+            realpath = None
+
+    return os.path.basename(executable) == os.path.basename(dexec) or executable == dexec or (realpath and compare_executable(realpath, de))
 
 def compare_cmdline(cmdline, de):
     dcmdline = de.get_commandline()
-    return os.path.basename(cmdline) == os.path.basename(dcmdline) or cmdline == dcmdline
+    ret = os.path.basename(cmdline) == os.path.basename(dcmdline) or cmdline == dcmdline
+
+    # try to handle interpreters like python
+    if not ret:
+        cmdargs = cmdline.split(" ");
+        if len(cmdargs) > 1:
+            ret = compare_executable(cmdargs[1], de)
+
+    return ret
+
+def compare_component(component, de):
+    dicon = de.get_icon()
+    if not dicon:
+        return False
+
+    if isinstance(dicon, Gio.ThemedIcon):
+        return component in dicon.get_names()
+    elif isinstance(dicon, Gio.FileIcon):
+        logging.debug("File icon: {0}".format(dicon.to_string()))
+        bf = os.path.basename(dicon.to_string())
+        if component == bf:
+            return True
+        elif '.' in bf:
+            component == bf[:bf.rindex('.')]
+        else:
+            return False
+    else:
+        logging.debug("Unsupported type of icon class: {0}".format(dicon))
+
+    return False
 
 def find_application(component, executable, cmdline):
     global __globa_app_cache__
 
-    lookupnames = [(cmdline, compare_cmdline), (executable, compare_executable)]
+    lookupnames = [(cmdline, compare_cmdline), (executable, compare_executable), (component, compare_component)]
 
     for n in lookupnames:
         if not n[0]:
             continue
 
         if n[0] in __globa_app_cache__:
-            return __globa_app_cache__[executable]
+            return __globa_app_cache__[n[0]]
 
         theme = Gtk.IconTheme.get_default()
         for dai in Gio.DesktopAppInfo.get_all():
@@ -66,17 +108,22 @@ def find_application(component, executable, cmdline):
                 icon = None
                 dai_icon = dai.get_icon()
                 if dai_icon:
-                    for name in dai_icon.get_names():
-                        try:
-                            icon = theme.load_icon(name, 128, Gtk.IconLookupFlags.USE_BUILTIN)
-                            break
-                        except gi._glib.GError as e:
-                            logging.debug(e.message)
+                    if isinstance(dai_icon, Gio.ThemedIcon):
+                        for name in dai_icon.get_names():
+                            try:
+                                icon = theme.load_icon(name, 128, Gtk.IconLookupFlags.USE_BUILTIN)
+                                break
+                            except gi._glib.GError as e:
+                                logging.debug(e.message)
+                    elif isinstance(dai_icon, Gio.FileIcon):
+                        stream = dai_icon.load(128, None)
+                        icon = GdkPixbuf.Pixbuf.new_from_stream(stream[0], None)
+                    else:
+                        logging.debug("Unsupported type of icon class: {0}".format(dai_icon))
 
-                __globa_app_cache__[executable] = Application(executable,
-                                                              name=dai.get_name(),
-                                                              icon=icon)
-                return __globa_app_cache__[executable]
+                __globa_app_cache__[n[0]] = Application(executable,
+                                                        name=dai.get_name(),
+                                                        icon=icon)
+                return __globa_app_cache__[n[0]]
 
-    __globa_app_cache__[executable] = Application(executable, name=component)
-    return __globa_app_cache__[executable]
+    return Application(executable if executable else "??", name=next(ifilter(lambda n: n, [component, executable, cmdline]), "??"))

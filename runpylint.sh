51d91b58ceeaa8d17873aa09c4a3d97817fb6144
Jakub Filak <jfilak@redhat.com>
2014-06-10 13:23:45 +0200

    Fix XDG_RUNTIME_DIR not set messages by creating one
    Closes rhbz#1106667
    
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

---
+++ runpylint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# This script will check gnome-abrt for any pylint warning and errors using a set
+# of options minimizing false positives.
+
+# XDG_RUNTIME_DIR is "required" to be set, so make one up in case something
+# actually tries to do something with it
+if [ -z "$XDG_RUNTIME_DIR" ]; then
+    export XDG_RUNTIME_DIR="$(mktemp -d)"
+    trap "rm -rf \"$XDG_RUNTIME_DIR\"" EXIT
+fi
+
+$PYLINT --rcfile=$PYLINTRC $@

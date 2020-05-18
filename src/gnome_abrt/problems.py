abrt/gnome-abrt

commit 837cb787caa4667a928673353e4ea8205ef9419a

problems: Use GTask to fetch report page titles
The current solution was broken, disabled, got more broken over time and
just in general is cumbersome. With GTask, we can avoid reinventing some
wheels.

This does require glib 2.63.0+ and gobject-introspection 1.63.0+(?),
since the used API is currently not introspectable.

Closes https://github.com/abrt/gnome-abrt/issues/25
Resolves https://github.com/abrt/gnome-abrt/issues/96

Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

src/gnome_abrt/problems.py
@@ -18,9 +18,19 @@
 import datetime
 import logging
 import re
+from urllib import request
+
+from bs4 import BeautifulSoup
+
+import gi
+gi.require_version('Gio', '2.0')
+from gi.repository import Gio
+gi.require_version('GLib', '2.0')
+from gi.repository import GLib
+gi.require_version('GObject', '2.0')
+from gi.repository import GObject
 
 # gnome-abrt
-import gnome_abrt.url
 from gnome_abrt.application import find_application
 from gnome_abrt.errors import (InvalidProblem,
                                UnavailableSource)
@@ -82,12 +92,6 @@ class Problem:
             self._data = data
             self._url_done = False
 
-        def _update_title_async_cb(self, result, unused_userdata):
-            if result[1]:
-                self._title = result[1]
-                self._problem.source.notify(ProblemSource.CHANGED_PROBLEM,
-                                            self._problem)
-
         @property
         def name(self):
             return self._name
@@ -108,8 +112,28 @@ class Problem:
         def title(self):
             if self._rtype == Problem.Submission.URL and not self._url_done:
                 self._url_done = True
-                gnome_abrt.url.get_url_title_async(self._data,
-                        self._update_title_async_cb, None)
+
+                def on_update_title(source_object, res, user_data):
+                    _, title = res.propagate_value()
+                    if title:
+                        self._title = title
+                        self._problem.source.notify(ProblemSource.CHANGED_PROBLEM,
+                                                    self._problem)
+
+                task = Gio.Task.new(None, None, on_update_title, None)
+
+                def update_title(task, source_object, task_data, cancellable):
+                    try:
+                        html = request.urlopen(self._data).read().decode("UTF-8")
+                        soup = BeautifulSoup(html, "html.parser")
+                        value = GObject.Value(str, soup.title.string)
+
+                        task.return_value(value)
+                    except Exception as ex:
+                        error = GLib.Error("Fetching title for problem report failed: %s" % (ex))
+                        task.return_error(error)
+
+                task.run_in_thread(update_title)
 
             return self._title
 

src/gnome-abrt
@@ -42,8 +42,6 @@ import gnome_abrt
 gnome_abrt.init()
 
 #pylint: disable=C0413
-import gnome_abrt.url
-#pylint: disable=C0413
 from gnome_abrt.views import OopsWindow
 #pylint: disable=C0413
 from gnome_abrt.controller import Controller
@@ -62,9 +60,6 @@ from gnome_abrt.errors import UnavailableSource
 from gnome_abrt.l10n import _, C_
 #pylint: disable=C0413
 from gnome_abrt.config import get_configuration
-#pylint: disable=C0413
-from gnome_abrt.url.gliburltitle import (GetURLTitleSourcePool,
-                                         GetURLTitleSourceCache)
 
 GNOME_ABRT_APPLICATION_ID = 'org.freedesktop.GnomeAbrt'
 GNOME_ABRT_INTERFACE = 'org.freedesktop.GnomeAbrt'
@@ -80,11 +75,6 @@ class OopsApplication(Gtk.Application):
 
         self.gcontext = None
         self.all_sources = None
-        self._url_pool = GetURLTitleSourcePool(GNOME_ABRT_URL_POOL_CAPACITY)
-        self._url_cache = GetURLTitleSourceCache(self._url_pool)
-        #gnome_abrt.url.set_async_worker(self._url_cache.get_url_title_async)
-        # Disable because of https://bugzilla.redhat.com/show_bug.cgi?id=959811
-        gnome_abrt.url.set_async_worker(lambda *args: None)
         self.gnome_settings = None
         # pylint: disable=W0105
         '''

src/gnome_abrt/meson.build
@@ -45,5 +45,4 @@ python_installation.install_sources(gnome_abrt_generated_sources + gnome_abrt_so
   subdir: 'gnome_abrt',
 )
 
-subdir('url')
 subdir('wrappers')

src/gnome_abrt/url/__init__.py
@@ -1,29 +1,0 @@
-# coding=UTF-8
-
-## Copyright (C) 2013 ABRT team <crash-catcher@lists.fedorahosted.org>
-## Copyright (C) 2013 Red Hat, Inc.
-
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335  USA
-
-from gnome_abrt.url import gliburltitle
-
-__ASYNC_WORKER = gliburltitle.get_url_title_async
-
-def set_async_worker(worker):
-    global __ASYNC_WORKER
-    __ASYNC_WORKER = worker
-
-def get_url_title_async(url, callback, user_data):
-    __ASYNC_WORKER(url, callback, user_data)

src/gnome_abrt/url/gliburltitle.py
@@ -1,177 +1,0 @@
-# coding=UTF-8
-
-## Copyright (C) 2013 ABRT team <crash-catcher@lists.fedorahosted.org>
-## Copyright (C) 2013 Red Hat, Inc.
-
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335  USA
-
-import sys
-from queue import Queue
-
-# pygobject
-#pylint: disable=E0611
-from gi.repository import GLib
-
-from gnome_abrt.url.urltitle import GetURLTitleThread
-
-
-def get_url_title_async(url, readycallback, userdata, context=None):
-    """Gets url's title asynchronously
-    """
-
-    if context is None:
-        context = GLib.main_context_default()
-
-    source = GetURLTitleThreadSource(url, readycallback, userdata)
-    #pylint: disable=E1101
-    source.attach(context)
-
-
-class GetURLTitleThreadSource(GLib.Source):
-    """Runs thread and waits until the thread does not return the url's title
-    """
-
-    #pylint: disable=W0613
-    def __new__(cls, *args):
-        return GLib.Source.__new__(cls)
-
-    def __init__(self, url, readycallback, userdata):
-        GLib.Source.__init__(self)
-
-        self._queue = Queue()
-        self._readycallback = readycallback
-        self._userdata = userdata
-        self._resolver = GetURLTitleThread(url, self._queue)
-        self._resolver.start()
-
-    def _is_ready(self):
-        return not self._queue.empty()
-
-    def prepare(self, *args):
-        return (self._is_ready(), 10)
-
-    def check(self, *args):
-        return self._is_ready()
-
-    def dispatch(self, *args):
-        if not self._is_ready():
-            raise RuntimeError("GetURLHTMLTitleThreadSource.dispatch()"
-            " called but result is not ready yet, or already consumed.")
-
-        # An attempty to destroy the resolver's thread ASAP
-        self._resolver = None
-
-        self._readycallback(self._queue.get(), self._userdata)
-
-        # Destroy this source
-        return False
-
-    def finalize(self, *args):
-        pass
-
-
-class GetURLTitleSourcePool:
-    """Pool for reducing number of running threads at time
-    """
-
-    def __init__(self, capacity):
-        self._capacity = capacity
-        self._running = 0
-        self._defered = list()
-
-    def get_url_title_async(self, url, readycallback, userdata):
-        if self._running == self._capacity:
-            self._defer_request(url, readycallback, userdata)
-        else:
-            self._start_resolving(url, readycallback, userdata)
-
-    def _defer_request(self, url, readycallback, userdata):
-        self._defered.append((url, readycallback, userdata))
-
-    def _start_resolving(self, url, readycallback, userdata):
-        self._running += 1
-        get_url_title_async(url, self._resolving_done,
-                (readycallback, userdata))
-
-    def _resolving_done(self, result, userdata):
-        try:
-            userdata[0](result, userdata[1])
-        finally:
-            self._running -= 1
-            if self._defered:
-                url, readycallback, userdata = self._defered.pop()
-                self._start_resolving(url, readycallback, userdata)
-
-
-class GetURLTitleSourceCache:
-    """Asynchronous cache for URL titles
-    """
-
-    def __init__(self, worker, max_size=1024):
-        self._worker = worker
-        self._max_size = max_size
-        self._cache = dict()
-        # The oldest is on the top
-        self._stamp = []
-
-    def get_url_title_async(self, url, readycallback, userdata):
-        if url in self._cache:
-            # Move url at the end
-            self._stamp.remove(url)
-            self._stamp.append(url)
-            # Callers expect that result will be delivered asynchronously
-            GLib.timeout_add(1,
-                lambda unused: readycallback((url, self._cache[url]), userdata),
-                None)
-            return
-
-        self._worker.get_url_title_async(url, self._resolved,
-                                         (readycallback, userdata))
-
-    def _resolved(self, result, userdata):
-        url, title = result
-        if title:
-            if len(self._cache.keys()) >= self._max_size:
-                # Remove the url on the top (the oldest)
-                del self._cache[self._stamp.pop()]
-
-            self._cache[url] = title
-            self._stamp.append(url)
-
-        readycallback, data = userdata
-        readycallback(result, data)
-
-
-if __name__ == "__main__":
-    def ready(result, userdata):
-        print(result[1] or result[0])
-
-        userdata["count"] = userdata["count"] - 1
-        if userdata["count"] == 0:
-            userdata["gloop"].quit()
-
-    URLS = ["http://docs.python.org/2/library/htmlparser.html"]
-
-    if len(sys.argv) > 1:
-        URLS = sys.argv[1:]
-
-    MAINLOOP = GLib.MainLoop()
-    WORKER = GetURLTitleSourcePool(2)
-    CONSUMER = {"count":len(URLS), "gloop":MAINLOOP}
-
-    for u in URLS:
-        WORKER.get_url_title_async(u, ready, CONSUMER)
-
-    MAINLOOP.run()

src/gnome_abrt/url/meson.build
@@ -1,17 +1,0 @@
-gnome_abrt_url_sources = [
-  '__init__.py',
-  'urltitle.py',
-  'gliburltitle.py',
-]
-
-foreach file: gnome_abrt_url_sources
-  configure_file(
-    copy: true,
-    input: file,
-    output: file,
-  )
-endforeach
-
-python_installation.install_sources(gnome_abrt_url_sources,
-  subdir: 'gnome_abrt/url',
-)

src/gnome_abrt/url/urltitle.py
@@ -1,142 +1,0 @@
-# coding=UTF-8
-
-## Copyright (C) 2013 ABRT team <crash-catcher@lists.fedorahosted.org>
-## Copyright (C) 2013 Red Hat, Inc.
-
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA  02110-1335  USA
-
-import sys
-import threading
-import logging
-from queue import Queue
-from urllib.request import urlopen
-from html.parser import HTMLParser
-
-
-def get_url_title(url):
-    """Returns a title of the url
-
-    Function expects that the url points to a HTML page with the title element
-    in the head element. Tries to download the page and return a value of the
-    title.
-    """
-
-    try:
-        urlio = urlopen(url)
-    except IOError as ex:
-        logging.debug("{1} ('{0}')".format(url, str(ex)))
-        return None
-
-    encoding = urlio.info().getparam('charset')
-    # Can't find any documentation but try except needed.
-    data = urlio.read()
-    if encoding:
-        try:
-            data = data.decode(encoding)
-        except UnicodeError as ex:
-            logging.debug("{1} ('{0}')".format(url, str(ex)))
-            return None
-
-    return HTMLTitleGetter.parse(data)
-
-
-class HTMLTitleGetter(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-
-        self._tag_head = False
-        self._tag_title = False
-        self._title = None
-
-    def handle_starttag(self, tag, attrs):
-        if not self._tag_head:
-            self._tag_head = tag.lower() == "head"
-
-        if self._tag_head and not self._tag_title:
-            self._tag_title = tag.lower() == "title"
-
-    def handle_endtag(self, tag):
-        if self._tag_head:
-            self._tag_head = tag.lower() != "head"
-
-        if self._tag_head and self._tag_title:
-            self._tag_title = tag.lower() != "title"
-
-    def handle_data(self, data):
-        if self._tag_title:
-            if not self._title:
-                self._title = data
-            else:
-                self._title += data
-
-    def error(self, message):
-        raise ValueError(message)
-
-    @staticmethod
-    def parse(data):
-        tgt = HTMLTitleGetter()
-        try:
-            tgt.feed(data)
-        #pylint: disable=W0703
-        except Exception as ex:
-            logging.debug("{1} ('{0}')".format(data, str(ex)))
-            # Hopefully title is parsed correctly
-
-        return tgt._title
-
-
-class GetURLTitleThread(threading.Thread):
-    """Simple thread which puts url's title into a Queue
-    """
-
-    def __init__(self, url, que):
-        super(GetURLTitleThread, self).__init__()
-
-        self._url = url
-        self._que = que
-
-    def run(self):
-        title = get_url_title(self._url)
-        self._que.put((self._url, title))
-
-
-if __name__ == "__main__":
-    URLS = ["http://docs.python.org/2/library/htmlparser.html"]
-
-    USE_THREADS = False
-    if len(sys.argv) > 1:
-        i = 1
-        if sys.argv[i] == "--threads":
-            USE_THREADS = True
-            i += 1
-        URLS = sys.argv[i:]
-
-    if USE_THREADS:
-        QUEUE = Queue()
-        THREADS = []
-        for u in URLS:
-            t = GetURLTitleThread(u, QUEUE)
-            t.start()
-            THREADS.append(t)
-
-        for t in THREADS:
-            t.join()
-
-        while not QUEUE.empty():
-            ORIG_URL, GOT_TITLE = QUEUE.get()
-            print(GOT_TITLE or ORIG_URL)
-    else:
-        for u in URLS:
-            print(get_url_title(u) or u)

commit 904ff11c5f2ca0c7ec1f99d56af2598bfa539f13

problems: Handle errors when fetching titles
Fallout from 837cb787caa4667a928673353e4ea8205ef9419a.

If any of the URIs in reported_to are inaccessible, the resulting HTTP
error will become an exception, which is totally unnecessary, because
the user will most likely have no idea what to do. Handling the
exception gracefully will just keep the dummy title.

This commit also narrows down the handled exceptions to GLib.Error,
since BeautifulSoup will eat whatever we feed it and not complain and
any other error should probably get some attention.

https://bugzilla.redhat.com/show_bug.cgi?id=1836614

src/gnome_abrt/problems.py
@@ -18,7 +18,8 @@
 import datetime
 import logging
 import re
-from urllib import request
+import sys
+import urllib
 
 from bs4 import BeautifulSoup
 
@@ -114,22 +115,25 @@ class Problem:
                 self._url_done = True
 
                 def on_update_title(source_object, res, user_data):
-                    _, title = res.propagate_value()
-                    if title:
-                        self._title = title
-                        self._problem.source.notify(ProblemSource.CHANGED_PROBLEM,
-                                                    self._problem)
+                    try:
+                        _, title = res.propagate_value()
+                        if title:
+                            self._title = title
+                            self._problem.source.notify(ProblemSource.CHANGED_PROBLEM,
+                                                        self._problem)
+                    except GLib.Error as error:
+                        print(error.message, file=sys.stderr)
 
                 task = Gio.Task.new(None, None, on_update_title, None)
 
                 def update_title(task, source_object, task_data, cancellable):
                     try:
-                        html = request.urlopen(self._data).read().decode("UTF-8")
+                        html = urllib.request.urlopen(self._data).read().decode("UTF-8")
                         soup = BeautifulSoup(html, "html.parser")
                         value = GObject.Value(str, soup.title.string)
 
                         task.return_value(value)
-                    except Exception as ex:
+                    except urllib.error.HTTPError as ex:
                         error = GLib.Error("Fetching title for problem report failed: %s" % (ex))
                         task.return_error(error)
 